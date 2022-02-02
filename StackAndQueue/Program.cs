5b6c52609197f481cbf3acb8c92d5597f8b975ad
[RajkumarVerma4124] <[vermar4124@gmail.com]>
2022-02-02 19:46:59 +0530

    [Raj] Added:- The welcome message in the main program

---
+++ StackAndQueue/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackAndQueue
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the stack and queue program");
+            Console.ReadLine();
+        }
+    }
+}
